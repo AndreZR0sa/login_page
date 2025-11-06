29db220c4f8a4d0f7f327517a707ca6f81c9a143
AndreZR0sa <andre.z.rosa@aluno.senai.br>
2025-11-06 17:18:18 -0300

    fall

---
+++ js/script.js
@@ -0,0 +1,53 @@
+document.addEventListener("DOMContentLoaded", function() {
+        const sendButton = document.getElementById("send");
+
+        sendButton.addEventListener("click", function() {
+            // Redireciona para a próxima página
+            window.location.href = "index2.html";
+        });
+    });
+
+document.addEventListener("DOMContentLoaded", function() {
+        const confirmBtn = document.getElementById("confirmBtn");
+
+        confirmBtn.addEventListener("click", function() {
+            // Redireciona para a próxima página
+            window.location.href = "index3.html";
+        });
+    });
+
+document.addEventListener("DOMContentLoaded", function() {
+        const close = document.getElementById("closeBtn");
+
+        close.addEventListener("click", function() {
+            // Redireciona para a próxima página
+            window.location.href = "index.html";
+        });
+    });
+
+document.addEventListener("DOMContentLoaded", function() {
+        const registerBtn = document.getElementById("registerBtn");
+
+        registerBtn.addEventListener("click", function() {
+            // Redireciona para a próxima página
+            window.location.href = "index4.html";
+        });
+    });
+
+document.addEventListener("DOMContentLoaded", function() {
+        const nextBtn = document.getElementById("nextBtn");
+
+        nextBtn.addEventListener("click", function() {
+            // Redireciona para a próxima página
+            window.location.href = "index5.html";
+        });
+    });
+
+document.addEventListener("DOMContentLoaded", function() {
+        const nextBtns = document.getElementById("nextBtns");
+
+        nextBtns.addEventListener("click", function() {
+            // Redireciona para a próxima página
+            window.location.href = "index6.html";
+        });
+    });
